fe4d4a875ea5395bb25dae5680e7f865b6c1bf45
ericjiang <my.cheng@msn.com>
2024-02-18 00:03:21 +0800

    update

--- python-client/tests/llms/test_openai.py
+++ python-client/tests/llms/test_openai.py
@@ -4,7 +4,7 @@ from aido_client.llms.openai import ChatOpenAI
 def test_invoke():
     chat_client = ChatOpenAI(
         model_name="gpt-3.5-turbo",
-        openai_api_key="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",
+        openai_api_key="sk-1234567890",
         openai_api_base="https://openai.aido.ai/v1"
     )
     response = chat_client.invoke("Hello, how are you?")
